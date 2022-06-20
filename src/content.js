162c40d89951515407226476300943bdcfb714a5
jmwalsh91 <jordanmwalsh@gmail.com>
2022-06-20 13:01:40 -0600

    feat: redify ads, targets cu-container class and tads id

--- src/content.js
+++ src/content.js
@@ -1,3 +1,29 @@
 //content scripts here
-
 console.log('content script ran')
+
+/**
+ * iterates over provided array of HTMLElements, changing background color of each element to red.
+ * @param {HTMLCollection} adsArray
+ */
+function redifyList(adsArray) {
+  for (i = 0; i < adsArray.length; i++) {
+    adsArray[i].style.backgroundColor = 'red'
+  }
+}
+/**
+ * gets element with where id = 'tads', then selects children div elements with data attribute 'data-text-ad'
+ * if ads is truthy, call {@link redifyList} with array of elements as arg. If falsy, log error
+ * TODO: error handling.
+ * @returns HTMLCollection of elements matching selector
+ */
+async function getAds() {
+  const adContainer = document.getElementById('tads')
+  adContainer && redifyList(adContainer.querySelectorAll('div[data-text-ad]'))
+
+  const shoppingAdContainer = document.getElementsByClassName('cu-container')
+  console.log(shoppingAdContainer)
+  shoppingAdContainer &&
+    redifyList(document.querySelectorAll('a[data-offer-id]'))
+}
+//invoke get ads
+getAds()
