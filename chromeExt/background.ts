c002dd1598d5c885f99a34fe1919126a293c3c97
jmwalsh91 <jordanmwalsh@gmail.com>
2022-06-23 23:59:06 -0600

    misc: capture progress, pivot to mv3 methodology

--- chromeExt/background.ts
+++ chromeExt/background.ts
@@ -1,2 +1,66 @@
 console.log('script ran')
+chrome.runtime.onMessage.addListener(function (message) {
+  switch (message.newVal) {
+    case true: 
+      message.append ? appendQuery(message.newVal) : null //update 
+    break;
+    case false:
+      return
+  }
+  return true 
+})
+
+chrome.runtime.onMessage.addListener(function (message) {
+  const queryStr = message.newVal
+  message.newVal.toLowerCase().includes('in 2021')
+    ? null
+    : appendQuery(queryStr)
+})
+function appendQuery(queryString: string) {
+  console.log()
+  
+  chrome.declarativeNetRequest.updateDynamicRules(
+    {
+      removeRuleIds: [0, 1, 2, 3, 4, 5, 6],
+      addRules: [
+        {
+          id: 0,
+          priority: 1,
+          condition: {
+            urlFilter: '*://www.google.com/*',
+          },
+          action: {
+            //Needed to directly provide enum member as value in type
+            type: chrome.declarativeNetRequest.RuleActionType.REDIRECT,
+            redirect: {
+              transform: {
+                queryTransform: {
+                  addOrReplaceParams: [
+                    { key: 'q', value: `${queryString} in 2021` },
+                  ],
+                },
+              },
+            },
+          },
+        },
+      ],
+    },
+    () => {
+      console.log(chrome.runtime.lastError)
+      console.log(chrome.declarativeNetRequest.getMatchedRules())
+      chrome.declarativeNetRequest.updateDynamicRules({
+        removeRuleIds: [5, 6],
+      })
+    }
+  )
+  /* const qInfo = {
+    text: queryString + ' in 2021',
+  }
+  chrome.search.query(qInfo, function () {
+    console.log(qInfo.text)
+  }) */
+}
+chrome.declarativeNetRequest.onRuleMatchedDebug.addListener(() => {
+  console.log('match')
+})
 export {}
--- chromeExt/content.ts
+++ chromeExt/content.ts
@@ -16,7 +16,9 @@ function redifyList(adsArray: NodeListOf<HTMLElement>) {
 }
 /**
  * Gets element with where id = 'tads', then selects children div elements with data attribute 'data-text-ad'.
+ *
  * If adContainer is truthy, call {@link redifyList} with array of elements as arg.
+ *
  * Gets element with classname 'cu-container', if truthy,   call {@link redifyList} with child <a> elements with data attribute 'data-offer-id' as arg.
  * @returns void
  */
@@ -33,14 +35,82 @@ function getAds() {
     document.getElementsByClassName('cu-container')
   shoppingAdContainer &&
     redifyList(
-      shoppingAdContainer[0].querySelectorAll(
-        'a[data-offer-id]'
-      ) as NodeListOf<HTMLElement>
+      document.querySelectorAll('a[data-offer-id]') as NodeListOf<HTMLElement>
     )
 }
 //invoke getAds
 getAds()
+
 /**
  * FIXME: Figure out config: TS needs export statement here to circumvent duplicate function implementation err, and chrome does not like the presence of an import or export statement in the compiled script --> current solution is to manually delete the export statement in the dist folder, which is certainly not ideal.
  */
+
+/**
+ * *************
+ *  APPEND QUERY
+ * **************
+ */
+/*********
+ * TYPES:
+ *********/
+type searchElement = HTMLInputElement
+interface searchValue {
+  [key: string]: HTMLInputElement['value']
+}
+/*************
+ * end types
+ * ***********
+ */
+/**
+ * sends message to service worker with value in searchbar.
+ * @param {searchValue} searchValue
+ */
+function dispatchSearchValToWorker(searchValue: searchValue) {
+  chrome.runtime.sendMessage(searchValue)
+}
+/**
+ * create and insert checkbox into DOM
+ * TODO: examine external state store during next feat and circle back.
+ */
+function checkboxTemplate() {
+  return `
+    <input 
+      type="checkbox"
+      
+      id="checkbox2021"
+      name="checkbox2021"
+      value="in 2021"
+      />
+      <label for="checkbox2021">
+        Search in 2021
+        </label>
+       `
+}
+/**
+ * getter for search bar textfield / input element.
+ *
+ * attaches listener to search button.
+ *
+ * dispatches ({@link dispatchSearchValToWorker}) message to service worker to initiate query modification.
+ */
+function attachSubmitListenerToForm() {
+  const searchField: searchElement | null = document.querySelector(
+    '[aria-label="Search"]'
+  )
+  const searchForm: HTMLElement | null = document.getElementById('tsf')
+
+  searchForm
+    ? (searchForm.onformdata = (e) => {
+        e.preventDefault()
+        console.log(e.formData)
+      })
+    : null
+  console.log(searchField)
+
+  searchField?.value
+    ? dispatchSearchValToWorker({ newVal: searchField.value } as searchValue)
+    : null
+}
+attachSubmitListenerToForm()
+
 export {}
