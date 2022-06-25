902aee5339914ae9864a15268c7cae85a7435187
jmwalsh91 <jordanmwalsh@gmail.com>
2022-06-24 22:16:09 -0600

    misc: test dev flow with HMR, add MUI + emotion

---
+++ src/components/AdTracker.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function AdTracker({}: Props) {
+  return (
+    <div>AdTracker</div>
+  )
+}
+
+export default AdTracker
--- src/components/PopUpBody.tsx
+++ src/components/PopUpBody.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import AdTracker from './AdTracker'
 
 function PopUpBody() {
-  return <h2>PopUpBody</h2>
+  return (
+    <div style={{ width: '300px' }}>
+      <h2>PopUpBody</h2>
+      <AdTracker/>
+    </div>
+  )
 }
 
 export default PopUpBody
